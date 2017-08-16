f0af3ec9a767d0dbaaab4c2d9f04bc4dd5048b73
Stephan Kulla <git.mail@kulla.me>
2017-08-16 14:31:35 +0200

    main script: Add global constant for target article

--- create_pdf_from_article.py
+++ create_pdf_from_article.py
@@ -5,12 +5,14 @@ import requests
 from api import HTTPMediaWikiAPI
 from cachecontrol import CacheControl
 
+ARTICLE = "Mathe für Nicht-Freaks: Was ist Analysis?"
+
 def run_script():
     """Runs this script."""
     req = CacheControl(requests.Session())
     api = HTTPMediaWikiAPI(req)
 
-    print(api.get_content("Mathe für Nicht-Freaks: Was ist Analysis?"))
+    print(api.get_content(ARTICLE))
 
 if __name__ == "__main__":
     run_script()
