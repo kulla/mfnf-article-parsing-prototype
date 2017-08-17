bce5117be660b71b12e66fce56b5d1c5c4791709
Stephan Kulla <git.mail@kulla.me>
2017-08-17 14:22:58 +0200

    Add function "lookup" in "utils.py"

---
+++ utils.py
@@ -0,0 +1,16 @@
+"""Module with utility functions."""
+
+def lookup(obj, *path):
+    """Lookups repeatedly the items in the list `path` of the object `obj`. In
+    case any `IndexError` or `KeyError` is thrown, `None` is returned. For
+    example the call `safe_lookup(obj, "a", 0, "b")` returns
+    `obj["a"][0]["b"]` when it exists and `None` otherwise."""
+    try:
+        result = obj
+
+        for key in path:
+            result = result[key]
+
+        return result
+    except (IndexError, KeyError):
+        return None
